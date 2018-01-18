739e83e10820fa0e3099dd65a9e7e2414aacba87
bonecrushereb <bonecrushereb@gmail.com>
2018-01-18 13:19:15 -0800

    change some fucntions to stateless as well as default props

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class InDecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     }
   }
     handleDeleteOptions() {
@@ -33,12 +33,11 @@ class InDecisionApp extends React.Component {
       });
     }
   render() {
-    const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle}/>
+        <Header subtitle={subtitle}/>
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick ={this.handlePick}
@@ -55,54 +54,55 @@ class InDecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return(
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+InDecisionApp.defaultProps = {
+  options: []
 }
 
-class Action extends React.Component {
-  render() {
-    return (
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+const Action = (props) => {
+  return (
     <div>
       <button
-         onClick={this.props.handlePick}
-         disabled={!this.props.hasOptions}
-         >
-           What should I do?
-       </button>
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+        >
+          What should I do?
+        </button>
+      </div>
+    );
+};
+
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>delete all options</button>
+        {
+          props.options.map((option) => <Option key={option} optionText={option} />)
+        }
     </div>
-    );
-  }
-}
+  );
+};
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>delete all options</button>
-          {
-            this.props.options.map((option) => <Option key={option} optionText={option} />)
-          }
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.optionText}
-      </div>
-    );
-  }
-}
+const Option = (props) =>  {
+  return (
+    <div>
+      {props.optionText}
+    </div>
+  );
+};
 
 class AddOption extends React.Component {
   constructor(props) {
