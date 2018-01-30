637aa279b32be4b13b3cdd699ad94c85d0025cd5
bonecrushereb <bonecrushereb@gmail.com>
2018-01-30 11:18:14 -0800

    breaking out into components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AddOption from './components/AddOption';
+import Option from './components/Option';
+
 class InDecisionApp extends React.Component {
   constructor(props) {
     super(props);
@@ -97,46 +103,5 @@ const Options = (props) => {
     </div>
   );
 };
-
-const Option = (props) => {
-  return (<div>
-    {props.optionText}
-    <button onClick={(e) => {
-        props.handleDeleteOption(props.optionText);
-      }}>remove</button>
-  </div>);
-};
-
-class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    }
-  }
-  handleAddOption(e) {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value.trim();
-    const error = this.props.handleAddOption(option);
-
-    this.setState(() => ({error}));
-
-    if(!error) {
-      e.target.elements.option.value = '';
-    }
-  }
-
-  render() {
-    return (<div>
-      {this.state.error && <p>{this.state.error}</p>}
-      <form onSubmit={this.handleAddOption}>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-      </form>
-    </div>);
-  }
-}
 
 ReactDOM.render(<InDecisionApp/>, document.getElementById('app'));
